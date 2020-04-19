4f53933266c6bbf0209a9ea03a2f754211a0055c
naveehima <60215514+naveehima@users.noreply.github.com>
2020-04-19 23:16:02 +0700

    Add files via upload

---
+++ myapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from myapp.models import UserProfileInfo, User
+from myapp.models import Docter
+# Register your models here.
+admin.site.register(UserProfileInfo)
+admin.site.register(Docter)
